8724fde95e3761f8a64257aea97fecafd74577f4
frontendeveloper01 <dundarfurkan03@gmail.com>
2024-10-03 13:43:36 +0300

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-let everyclick = 0
 
-const clickerShow = document.getElementById('clickerShow')
-const btn = document.getElementById('btn')
-const azalt = document.getElementById('dicrease')
-
-btn.addEventListener("click",()=>{
-    everyclick++;
-    clickerShow.innerHTML = `${everyclick}`
-    
-    console.log(everyclick)
-})
-
-azalt.addEventListener("click",()=> {
-    everyclick--;
-    clickerShow.innerHTML = `${everyclick}`
-})
